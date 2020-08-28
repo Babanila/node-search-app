8405ff41eee537682af5bee107318b6ae4c5a36a
Babanila <babanila@yahoo.com>
2020-08-28 11:25:53 +0200

    feat(): add to my repo

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
-const formatLanguages = (languageInput = "") => {
-  const languageArray = languageInput.split(",");
-  return languageArray.map((item) => `language:"${item}"`);
-};
+const formatLanguages = (languageInput = '') => {
+  const languageArray = languageInput.split(',')
+  return languageArray.map((item) => `language:"${item}"`)
+}
 
-module.exports = { formatLanguages };
+module.exports = { formatLanguages }
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,17 +1,14 @@
-const { formatLanguages } = require("./utils");
+const { formatLanguages } = require('./utils')
 
-describe("Check formatLanguages", () => {
-  it("should output a single language", () => {
-    const languages = "javascript";
-    expect(formatLanguages).toBeDefined();
-    expect(formatLanguages(languages)).toEqual([`language:"javascript"`]);
-  });
-  it("should output multiple languages", () => {
-    const languages = "java,javascript";
-    expect(formatLanguages).toBeDefined();
-    expect(formatLanguages(languages)).toEqual([
-      `language:"java"`,
-      `language:"javascript"`,
-    ]);
-  });
-});
+describe('Check formatLanguages', () => {
+  it('should output a single language', () => {
+    const languages = 'javascript'
+    expect(formatLanguages).toBeDefined()
+    expect(formatLanguages(languages)).toEqual([`language:"javascript"`])
+  })
+  it('should output multiple languages', () => {
+    const languages = 'java,javascript'
+    expect(formatLanguages).toBeDefined()
+    expect(formatLanguages(languages)).toEqual([`language:"java"`, `language:"javascript"`])
+  })
+})
